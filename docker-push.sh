d69c13ca13d159a0ea7aae8d5da586939b02a29b
Sergio Fernández <sergio@wikier.org>
2016-01-13 14:15:29 +0100

    removed old image after squashing

--- docker-push.sh
+++ docker-push.sh
@@ -19,5 +19,8 @@ IMAGE_ID=$(docker images -q $IMAGE)
 echo "Squashing image ${IMAGE} (${IMAGE_ID})..."
 docker save ${IMAGE_ID} | sudo docker-squash -t ${IMAGE} | docker load
 
+echo "Removing old image (${IMAGE_ID})..."
+docker rmi ${IMAGE_ID}
+
 echo "Pushing image ${IMAGE} (${IMAGE_ID})..."
 docker push ${IMAGE}
